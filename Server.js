0f71ebc89bc536b3b480f20693b8b6b42f720ebd
Gabriel Coelho <gabriellvs60@gmail.com>
2019-11-25 22:14:39 -0300

    Curso de  React+Apollo+GraphQL
    Curso de  React+Apollo+GraphQL

--- Server.js
+++ Server.js
@@ -1,20 +1,57 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config({path: 'variables.env'});
-const Recipe = require('./models/Recipe');
-const User = require('./models/User');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config({ path: "variables.env" });
+const Recipe = require("./models/Recipe");
+const User = require("./models/User");
 
-//Connects to database
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log('DB Connected'))
-.catch(err => console.error(err));
+// Bring in GraphQL-Express middleware
+const { graphiqlExpress, graphqlExpress } = require("apollo-server-express");
+const { makeExecutableSchema } = require("graphql-tools");
 
-//Initializes Application
+const { typeDefs } = require("./schema");
+const { resolvers } = require("./resolvers");
+
+// Create schema
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+// Connects to database
+mongoose
+  .connect(process.env.MONGO_URI,{autoIndex: false})
+  .then(() => console.log("DB connected"))
+  .catch(err => console.error(err));
+
+// Initializes application
 const app = express();
 
-const PORT = process.env.PORT || 444;
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true
+};
+app.use(cors(corsOptions));
+
+// Create GraphiQL application
+app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
+
+// Connect schemas with GraphQL
+app.use(
+  "/graphql",
+  bodyParser.json(),
+  graphqlExpress({
+    schema,
+    context: {
+      Recipe,
+      User
+    }
+  })
+);
+
+const PORT = process.env.PORT || 4444;
 
 app.listen(PORT, () => {
-    console.log(`server listening on 
-    ${PORT}`);
-})
+  console.log(`Server listening on PORT ${PORT}`);
+});
--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new Schema({
   username: {
@@ -26,4 +26,19 @@ const UserSchema = new Schema({
   }
 });
 
-module.exports = mongoose.model('User', UserSchema);
+UserSchema.pre("save", function(next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) return next(err);
+      this.password = hash;
+      next();
+    });
+  });
+});
+
+module.exports = mongoose.model("User", UserSchema);
